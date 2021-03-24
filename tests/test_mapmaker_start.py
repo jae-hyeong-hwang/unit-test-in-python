456dc11c648528637df88eb35e048c5ab9206160
JAE HYEONG HWANG <71170784+jae-hyeong-hwang@users.noreply.github.com>
2021-03-24 15:45:06 +0100

    Update test_mapmaker_start.py

--- tests/test_mapmaker_start.py
+++ tests/test_mapmaker_start.py
@@ -1,6 +1,7 @@
 from scripts.mapmaker_start import Point
 
+# in order to prevent minor bug add blank line
 def test_make_one_point():
     p1 = Point("Dakar", 14.7167, 17.4677)
     assert p1.get_lat_long() == (14.7167, 17.4677)
-    
+# in order to prevent minor bug add blank line    
