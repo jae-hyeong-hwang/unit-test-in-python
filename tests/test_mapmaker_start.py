ee3bc68aba44cdc9358e279ba04ab3dc4ec0efaa
JAE HYEONG HWANG <71170784+jae-hyeong-hwang@users.noreply.github.com>
2021-03-25 01:41:17 +0100

    Update test_mapmaker_start.py

--- tests/test_mapmaker_start.py
+++ tests/test_mapmaker_start.py
@@ -1,7 +1,15 @@
 from scripts.mapmaker_start import Point
+import pytest
 
 # in order to prevent minor bug add blank line
 def test_make_one_point():
     p1 = Point("Dakar", 14.7167, 17.4677)
     assert p1.get_lat_long() == (14.7167, 17.4677)
-# in order to prevent minor bug add blank line    
+# in order to prevent minor bug add blank line 
+
+def test_invalid_point_generation():
+    with pytest.raises(ValueError) as exp:
+        Point("Buenos Aires", 12.11386, -555.08269)
+    #breakpoint()
+    assert str(exp.value) == 'Invalid latitude, longitude combination'
+    
